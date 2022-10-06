36e39c743a251cb89d99addbf7058dbe7ca90cd8
Vishal Choudhary <contactvishaltech@gmail.com>
2022-10-06 23:42:42 +0530

    allowed comm bw clients
    Signed-off-by: Vishal Choudhary <contactvishaltech@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,38 @@
 use tokio::{
-    io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader},
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::TcpListener,
+    sync::broadcast,
 };
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
+    let (tx, rx) = broadcast::channel::<String>(10);
+
     loop {
         let (mut socket, _addr) = listener.accept().await.unwrap();
+
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
 
         tokio::spawn(async move {
             let (reader, mut writer) = socket.split();
 
             let mut reader = BufReader::new(reader);
             let mut line = String::new();
+
             loop {
                 let bytes_read = reader.read_line(&mut line).await.unwrap();
                 if bytes_read == 0 {
                     break;
                 }
 
-                writer.write_all(line.as_bytes()).await.unwrap();
+                tx.send(line.clone()).unwrap();
+
+                let msg = rx.recv().await.unwrap();
+
+                writer.write_all(msg.as_bytes()).await.unwrap();
                 line.clear();
             }
         });
